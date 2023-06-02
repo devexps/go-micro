4bff13a31ef04e81983cf64bdc0beb58f4e3ce1b
thangn <thangn@vng.com.vn>
2023-06-02 10:20:51 +0700

    feat: add recovery middleware

---
+++ middleware/recovery/recovery.go
@@ -0,0 +1,62 @@
+package recovery
+
+import (
+	"context"
+	"runtime"
+	"time"
+
+	"github.com/devexps/go-micro/v2/errors"
+	"github.com/devexps/go-micro/v2/log"
+	"github.com/devexps/go-micro/v2/middleware"
+)
+
+// Latency is recovery latency context key
+type Latency struct{}
+
+// ErrUnknownRequest is unknown request error.
+var ErrUnknownRequest = errors.InternalServer("UNKNOWN", "unknown request error")
+
+// HandlerFunc is recovery handler func.
+type HandlerFunc func(ctx context.Context, req, err interface{}) error
+
+// Option is recovery option.
+type Option func(*options)
+
+type options struct {
+	handler HandlerFunc
+}
+
+// WithHandler with recovery handler.
+func WithHandler(h HandlerFunc) Option {
+	return func(o *options) {
+		o.handler = h
+	}
+}
+
+// Recovery is a server middleware that recovers from any panics.
+func Recovery(opts ...Option) middleware.Middleware {
+	op := options{
+		handler: func(ctx context.Context, req, err interface{}) error {
+			return ErrUnknownRequest
+		},
+	}
+	for _, o := range opts {
+		o(&op)
+	}
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			startTime := time.Now()
+			defer func() {
+				if rerr := recover(); rerr != nil {
+					buf := make([]byte, 64<<10) //nolint:gomnd
+					n := runtime.Stack(buf, false)
+					buf = buf[:n]
+					log.Context(ctx).Errorf("%v: %+v\n%s\n", rerr, req, buf)
+					ctx = context.WithValue(ctx, Latency{}, time.Since(startTime).Seconds())
+					err = op.handler(ctx, req, rerr)
+				}
+			}()
+			return handler(ctx, req)
+		}
+	}
+}
