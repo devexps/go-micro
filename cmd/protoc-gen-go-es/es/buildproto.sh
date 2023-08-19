ee4261abed73e996e73e53f045dcc03739da469d
devexps <dev.experiences@gmail.com>
2023-08-19 12:07:30 +0700

    refactor(protoc-gen-go-es): remove unused files

--- cmd/protoc-gen-go-es/es/buildproto.sh
+++
@@ -1,6 +0,0 @@
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
-  --go_out=. --go-grpc_opt=paths=source_relative --go_opt=paths=source_relative --go-grpc_out=. --grpc-gateway_out=logtostderr=true:. \
-  *.proto
