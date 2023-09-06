2356e2346214f00aa01b7579999ec9241ce79866
devexps <dev.experiences@gmail.com>
2023-09-06 12:12:05 +0700

    feat: add unit test for logging pkg

---
+++ log/filter_test.go
@@ -0,0 +1,120 @@
+package log
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestFilterAll(_ *testing.T) {
+	logger := With(DefaultLogger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	log := NewHelper(NewFilter(logger,
+		FilterLevel(LevelDebug),
+		FilterKey("username"),
+		FilterValue("hello"),
+		FilterFunc(testFilterFunc),
+	))
+	log.Log(LevelDebug, "msg", "test debug")
+	log.Info("hello")
+	log.Infow("password", "123456")
+	log.Infow("username", "go-micro")
+	log.Warn("warn log")
+}
+
+func TestFilterLevel(_ *testing.T) {
+	logger := With(DefaultLogger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	log := NewHelper(NewFilter(NewFilter(logger, FilterLevel(LevelWarn))))
+	log.Log(LevelDebug, "msg1", "te1st debug")
+	log.Debug("test debug")
+	log.Debugf("test %s", "debug")
+	log.Debugw("log", "test debug")
+	log.Warn("warn log")
+}
+
+func TestFilterCaller(_ *testing.T) {
+	logger := With(DefaultLogger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	log := NewFilter(logger)
+	_ = log.Log(LevelDebug, "msg1", "te1st debug")
+	logHelper := NewHelper(NewFilter(logger))
+	logHelper.Log(LevelDebug, "msg1", "te1st debug")
+}
+
+func TestFilterKey(_ *testing.T) {
+	logger := With(DefaultLogger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	log := NewHelper(NewFilter(logger, FilterKey("password")))
+	log.Debugw("password", "123456")
+}
+
+func TestFilterValue(_ *testing.T) {
+	logger := With(DefaultLogger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	log := NewHelper(NewFilter(logger, FilterValue("debug")))
+	log.Debugf("test %s", "debug")
+}
+
+func TestFilterFunc(_ *testing.T) {
+	logger := With(DefaultLogger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	log := NewHelper(NewFilter(logger, FilterFunc(testFilterFunc)))
+	log.Debug("debug level")
+	log.Infow("password", "123456")
+}
+
+func TestFilterFuncWitchLoggerPrefix(t *testing.T) {
+	buf := new(bytes.Buffer)
+	tests := []struct {
+		logger Logger
+		want   string
+	}{
+		{
+			logger: NewFilter(With(NewStdLogger(buf), "caller", "caller", "prefix", "whaterver"), FilterFunc(testFilterFuncWithLoggerPrefix)),
+			want:   "",
+		},
+		{
+			// Filtered value
+			logger: NewFilter(With(NewStdLogger(buf), "caller", "caller"), FilterFunc(testFilterFuncWithLoggerPrefix)),
+			want:   "INFO caller=caller msg=msg filtered=***\n",
+		},
+		{
+			// NO prefix
+			logger: NewFilter(With(NewStdLogger(buf)), FilterFunc(testFilterFuncWithLoggerPrefix)),
+			want:   "INFO msg=msg filtered=***\n",
+		},
+	}
+
+	for _, tt := range tests {
+		err := tt.logger.Log(LevelInfo, "msg", "msg", "filtered", "true")
+		if err != nil {
+			t.Fatal("err should be nil")
+		}
+		got := buf.String()
+		if got != tt.want {
+			t.Fatalf("filter should catch prefix, want %s, got %s.", tt.want, got)
+		}
+		buf.Reset()
+	}
+}
+
+func testFilterFunc(level Level, keyvals ...interface{}) bool {
+	if level == LevelWarn {
+		return true
+	}
+	for i := 0; i < len(keyvals); i++ {
+		if keyvals[i] == "password" {
+			keyvals[i+1] = fuzzyStr
+		}
+	}
+	return false
+}
+
+func testFilterFuncWithLoggerPrefix(level Level, keyvals ...interface{}) bool {
+	if level == LevelWarn {
+		return true
+	}
+	for i := 0; i < len(keyvals); i += 2 {
+		if keyvals[i] == "prefix" {
+			return true
+		}
+		if keyvals[i] == "filtered" {
+			keyvals[i+1] = fuzzyStr
+		}
+	}
+	return false
+}
---
+++ log/log_test.go
@@ -0,0 +1,17 @@
+package log
+
+import (
+	"context"
+	"testing"
+)
+
+func TestInfo(_ *testing.T) {
+	logger := DefaultLogger
+	logger = With(logger, "ts", DefaultTimestamp)
+	logger = With(logger, "caller", DefaultCaller)
+	_ = logger.Log(LevelInfo, "key1", "value1")
+}
+
+func TestWithContext(_ *testing.T) {
+	WithContext(context.Background(), nil)
+}
